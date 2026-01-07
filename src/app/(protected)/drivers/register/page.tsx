db3b21d1fbbdbb29d439d1e5e0658e1bcda5d592
Bhanuteja <nallamothubhanuteja110@gmail.com>
2026-01-07 11:05:55 +0000

    do same for Register Driver page

--- src/app/(protected)/drivers/register/page.tsx
+++ src/app/(protected)/drivers/register/page.tsx
@@ -83,7 +83,7 @@ export default function RegisterDriverPage() {
   }
 
   return (
-    <Card>
+    <Card className="bg-card/50">
       <CardHeader>
         <CardTitle>Register New Driver</CardTitle>
         <CardDescription>
